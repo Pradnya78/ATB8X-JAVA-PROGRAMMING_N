aca7291408163a22feeb5340baf93e472e6e2671
Pradnya Karle <pradnyakarle@gmail.com>
2026-01-02 11:05:11 +0530

    First program

---
+++ src/JAN/ex_020126/Lab001.java
@@ -0,0 +1,10 @@
+package JAN.ex_020126;
+
+public class Lab001 {
+    public static void main(String[] args)
+    {
+        System.out.println("hello world!");
+        System.out.println(7+12);
+
+    }
+}
